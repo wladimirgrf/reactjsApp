338279ecc000659b1be458f3c964bdf40d6fdd1a
Wladimir <wladimirgrf@gmail.com>
2020-02-24 15:19:40 -0300

    Main page structure, react icons, layout

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import { FaGithubAlt, FaPlus } from 'react-icons/fa';
 
-import { Title } from './styles';
+import { Container, Form, SubmitButton } from './styles';
 
 function Main() {
-  return <Title>Main</Title>;
+  return (
+    <Container>
+      <h1>
+        <FaGithubAlt />
+        Repositórios
+      </h1>
+      <Form onSubmit={() => {}}>
+        <input type="text" placeholder="Adicionar repositório" />
+
+        <SubmitButton>
+          <FaPlus color="#fff" size={14} />
+        </SubmitButton>
+      </Form>
+    </Container>
+  );
 }
 
 export default Main;
